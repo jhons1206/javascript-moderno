24ca1b6ffe66323b34b61e9d90cfe95addc6c89f
jhons1206 <webdeveloper84@gmail.com>
2022-11-01 09:01:36 -0500

    Initial commit

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,33 @@
+// alert("Hola Mundo");
+
+// const nombre = prompt('Cual es tu nombre?');
+
+// Recibe el nombre y lo muestra en una tabla
+// document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo Javascript Moderno`;
+
+// Asigna el valor hacia la variable producto
+const producto = 'Monitor 24 pulgadas';
+
+console.error('Algo salió mal');
+console.warn('Algo salió mal');
+
+console.time('Hola');
+
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+console.warn('Eso no esta permitido');
+
+console.timeEnd('Hola');
+
+// Crear un objeto
+const obj = {
+    nombre: "Jhon",
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# JavaScript Moderno Guía Definitiva Construye +20 Proyectos
+![Landing Page Semana del Voluntariado](cover_course_js.png)
+###### Instructor: Código con Juan
+Inicio: 31/10/2022
