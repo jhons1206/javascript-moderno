7be207a42753a9b8998573e33d0af824f2001d6d
Jhon Sandoval <webdeveloper84@gmail.com>
2023-01-04 18:23:51 -0500

    Seccion 18: LocalStorage en JavaScript - Cap. 153

---
+++ 18-LocalStorage/js/01-app.js
@@ -0,0 +1,20 @@
+localStorage.setItem('nombre', 'Jhon');
+
+// sessionStorage.setItem('nombre', 'Juan');
+
+const producto = {
+    nombre: 'Monitor 24 pulgadas',
+    precio: 500
+}
+
+// Objeto
+const productoString = JSON.stringify( producto );
+// console.log(productoString);
+localStorage.setItem('producto', productoString);
+
+
+// Arreglo
+const meses = ['Enero', 'Febrero', 'Marzo'];
+const mesesString = JSON.stringify ( meses );
+localStorage.setItem('meses', mesesString);
+// localStorage.setItem('meses2', JSON.stringify ( meses ));
