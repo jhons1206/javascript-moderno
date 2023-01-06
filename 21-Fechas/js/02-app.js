d44b491091d344eb0048fd2bbc4dae29fefbdc45
Jhon Sandoval <webdeveloper84@gmail.com>
2023-01-06 16:23:59 -0500

    Seccion 21: Fechas en JavaScript - COMPLETADO

--- 21-Fechas/js/02-app.js
+++ 21-Fechas/js/02-app.js
@@ -1,2 +1,11 @@
 const diaHoy = new Date(); // Date es un objeto
 
+moment.locale('es');
+
+console.log( moment().format('MMMM D YYYY h:mm:ss a') );
+
+
+console.log( moment().format('LLLL', diaHoy) );
+
+
+console.log( moment().add(3, 'days').calendar() );
