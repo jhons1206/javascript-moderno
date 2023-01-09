4651d4feea78d61ff96b3256ceef42cd3d9c3ff4
jhons1206 <webdeveloper84@gmail.com>
2023-01-09 09:12:31 -0500

    Seccion 23: PROYECTO: Cotizador con Prototypes - Cap. 171

---
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -0,0 +1,55 @@
+
+// Constructores
+function Seguro(marca, year, tipo) {
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
+}
+
+function UI() {}
+
+// Llena las opciones de los años
+UI.prototype.llenarOpciones = () => {
+    const max = new Date().getFullYear(),
+          min = max - 20;
+
+    const selectYear = document.querySelector('#year');
+
+    for(let i = max; i > min; i--) {
+        let option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        selectYear.appendChild(option);
+    }
+}
+
+// Instanciar UI
+const ui = new UI();
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    ui.llenarOpciones(); // Llena el select con los años
+})
+
+
+
+eventListeners();
+function eventListeners() {
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarSeguro);
+}
+
+function cotizarSeguro(e) {
+    e.preventDefault();
+
+    // Leer la marca seleccionada
+    const marca = document.querySelector('#marca');
+
+    // Leer el año seleccionado
+    const year = document.querySelector('#year');
+
+    // Leer el tipo de cobertura
+    
+
+    console.log('Cotizando...');
+}
