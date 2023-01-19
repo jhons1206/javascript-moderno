9bff7896383575390b5edc5d3354bc97197dc620
jhons1206 <webdeveloper84@gmail.com>
2023-01-18 20:22:49 -0500

    Seccion 25: PROYECTO: Administracion de Presupuesto con Classes - COMPLETADO

--- 25-PROYECTO-GastoSemanal/js/app.js
+++ 25-PROYECTO-GastoSemanal/js/app.js
@@ -1,10 +1,10 @@
-// Variables y selectores
+///-- Variables y selectores ///
 const formulario = document.querySelector('#agregar-gasto');
 const gastoListado = document.querySelector('#gastos ul');
 
 
 
-// Eventos
+///-- Eventos ///
 eventListeners();
 function eventListeners() {
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto );
@@ -14,7 +14,8 @@ function eventListeners() {
 
 
 
-// Classes
+///-- Classes ///
+
 class Presupuesto {
     constructor(presupuesto) {
         this.presupuesto = Number(presupuesto);
@@ -30,6 +31,13 @@ class Presupuesto {
     calcularRestante() {
         const gastado = this.gastos.reduce( ( total, gasto ) => total + gasto.cantidad, 0 );
         this.restante = this.presupuesto - gastado;
+    }
+
+    eliminarGasto(id) {
+        this.gastos = this.gastos.filter( gasto => gasto.id !== id );
+        this.calcularRestante();
+
+        // console.log(this.gastos);
     }
 }
 
@@ -66,7 +74,7 @@ class UI {
         }, 3000);
     }
 
-    agregarGastoListado(gastos) {
+    mostrarGastos(gastos) {
 
         this.limpiarHTML(); // Elimina el HTML previo
 
@@ -89,6 +97,9 @@ class UI {
             const btnBorrar = document.createElement('button');
             btnBorrar.classList.add('btn', 'btn-danger', 'borrar-gasto');
             btnBorrar.textContent = 'Borrar X';
+            btnBorrar.onclick = () => {
+                eliminarGasto(id);
+            }
             nuevoGasto.appendChild(btnBorrar);
 
             // Agregar al HTML
@@ -118,6 +129,9 @@ class UI {
         } else if ( (presupuesto / 2) > restante ) {
             restanteDiv.classList.remove('alert-success');
             restanteDiv.classList.add('alert-warning');
+        } else {
+            restanteDiv.classList.remove('alert-danger', 'alert-warning');
+            restanteDiv.classList.add('alert-success');
         }
 
         // Si el total es 0 o menor
@@ -137,7 +151,8 @@ let presupuesto;
 
 
 
-// Funciones
+///-- Funciones ///
+
 function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('¿Cuál es tu presupuesto?');
 
@@ -182,7 +197,7 @@ function agregarGasto(e) {
 
     // Imprimir los gastos
     const { gastos, restante } = presupuesto;
-    ui.agregarGastoListado(gastos);
+    ui.mostrarGastos(gastos);
 
     ui.actualizarRestante(restante);
 
@@ -193,3 +208,14 @@ function agregarGasto(e) {
 
 }
 
+
+function eliminarGasto(id) {
+    // Elimina gastos del objeto
+    presupuesto.eliminarGasto(id);
+
+    // Elimina los gastos del HTML
+    const { gastos, restante } = presupuesto;
+    ui.mostrarGastos(gastos);
+    ui.actualizarRestante(restante);
+    ui.comprobarPresupuesto(presupuesto);
+}
