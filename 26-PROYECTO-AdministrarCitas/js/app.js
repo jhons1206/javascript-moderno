952e3d08b69d91bf75305f3f586ce52e7b81a3ba
jhons1206 <webdeveloper84@gmail.com>
2023-01-19 08:46:08 -0500

    Sección 26: PROYECTO: Administrador de Citas de Pacientes - Cap. 192

---
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -0,0 +1,116 @@
+///-- Variables y selectores ///
+
+// Campos del formulario
+const mascotaInput = document.querySelector('#mascota');
+const propietarioInput = document.querySelector('#propietario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
+
+// UI
+const formulario = document.querySelector('#nueva-cita');
+const contenedorCitas = document.querySelector('#citas');
+
+
+///-- Eventos ///
+
+// Registrar eventos
+
+eventListeners();
+function eventListeners() {
+    mascotaInput.addEventListener('input', datosCita);
+    propietarioInput.addEventListener('input', datosCita);
+    telefonoInput.addEventListener('input', datosCita);
+    fechaInput.addEventListener('input', datosCita);
+    horaInput.addEventListener('input', datosCita);
+    sintomasInput.addEventListener('input', datosCita);
+
+    formulario.addEventListener('submit', nuevaCita);
+}
+
+
+///-- Classes ///
+
+class Citas {
+    constructor() {
+        this.citas = [];
+    }
+}
+
+class UI {
+
+    imprimirAlerta(mensaje, tipo) {
+        // Crear el div
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert', 'd-block', 'col-12');
+
+        // Agregar clase en base al tipo de error
+        if(tipo === 'error') {
+            divMensaje.classList.add('alert-danger');
+        } else {
+            divMensaje.classList.add('alert-success');
+        }
+
+        // Mensaje de error
+        divMensaje.textContent = mensaje;
+
+        // Agregar al HTML
+        document.querySelector('#contenido').insertBefore(divMensaje, document.querySelector('.agregar-cita'));
+
+        // Quitar la alerta después de 5 segundos
+        setTimeout( () => {
+            divMensaje.remove();
+        }, 3000 );
+    }
+
+}
+
+// Instanciamos las clases
+const ui = new UI();
+
+const administrarCitas = new Citas();
+
+
+
+
+///-- Funciones ///
+
+// Objeto con la información de la cita
+const citaObj = {
+    mascota: '',
+    propietario: '',
+    telefono: '',
+    fecha: '',
+    hora: '',
+    sintomas: ''
+}
+
+// Agrega datos al objeto de cita
+function datosCita(e) {
+    citaObj[e.target.name] = e.target.value;
+}
+
+
+// Valida y agrega una nueva cita a la clase de citas
+function nuevaCita(e) {
+    e.preventDefault();
+
+    // Extraer la información del objeto de cita
+    const { mascota, propietario, telefono, fecha, hora, sintomas } = citaObj;
+
+    // Validar campos vacíos
+    if( mascota === '' || propietario === '' || telefono === '' || fecha === '' || hora === '' || sintomas === '' ) {
+        ui.imprimirAlerta('Todos los campos son obligatorios', 'error');
+
+        return;
+    }
+
+    // Creando una nueva cita
+    
+
+
+
+
+}
+
