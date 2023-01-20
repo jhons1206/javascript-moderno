18189a63553f94beb93ccd1c0c7b8f957a8470f5
jhons1206 <webdeveloper84@gmail.com>
2023-01-20 09:04:31 -0500

    Sección 26: PROYECTO: Administrador de Citas de Pacientes - Cap. 196

--- 26-PROYECTO-AdministrarCitas/js/app.js
+++ 26-PROYECTO-AdministrarCitas/js/app.js
@@ -120,6 +120,13 @@ class UI {
 
             btnEliminar.onclick = () => eliminarCita(id);
 
+            // Boton para editar esta cita
+            const btnEditar = document.createElement('button');
+            btnEditar.classList.add('btn', 'btn-info');
+            btnEditar.innerHTML = 'Editar <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" /></svg>';
+
+            btnEditar.onclick = () => cargarEdicion(cita);
+
 
             // Agregar lso parrafos al divCita
             divCita.appendChild(mascotaParrafo);
@@ -129,6 +136,7 @@ class UI {
             divCita.appendChild(horaParrafo);
             divCita.appendChild(sintomasParrafo);
             divCita.appendChild(btnEliminar);
+            divCita.appendChild(btnEditar);
 
             // Agregar las citas al HTML
             contenedorCitas.appendChild(divCita);
@@ -218,4 +226,10 @@ function eliminarCita(id) {
 
     // Refrescar las citas
     ui.imprimirCitas(administrarCitas);
+}
+
+
+// Cargar los datos y el modo edició
+function cargarEdicion(cita) {
+    console.log(cita);
 }
