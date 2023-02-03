4a82e8c725932c455ff113b829985f16af609ce8
Jhon Sandoval <webdeveloper84@gmail.com>
2023-02-03 18:04:52 -0500

    Sección 37: PROYECTO: App de Recetas con Fetch API y LocalStorage - Cap. 259

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -1,7 +1,9 @@
 function iniciarApp() {
 
     const selectCategorias = document.querySelector('#categorias');
-    selectCategorias.addEventListener('change', selectCategorias);
+    selectCategorias.addEventListener('change', seleccionarCategoria);
+
+    const resultado = document.querySelector('#resultado');
 
     obtenerCategorias();
 
@@ -15,7 +17,7 @@ function iniciarApp() {
     function mostrarCategorias( categorias = []) {
         categorias.forEach( categoria => {
             const option = document.createElement('OPTION');
-            console.log(option);
+            // console.log(option);
 
             const { strCategory } = categoria;
 
@@ -23,6 +25,60 @@ function iniciarApp() {
             option.textContent = strCategory;
             selectCategorias.appendChild(option);
         })
+    }
+
+    function seleccionarCategoria(e) {
+        const categoria = e.target.value;
+        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
+
+        fetch(url)
+            .then( respuesta => respuesta.json())
+            .then( resultado => mostrarRecetas(resultado.meals))
+    }
+
+    function mostrarRecetas(recetas = []) {
+
+        // Iterar en los resultados
+        recetas.forEach( receta => {
+            // console.log(receta);
+            const { idMeal, strMeal, strMealThumb } = receta;
+
+            const recetaContenedor = document.createElement('DIV');
+            recetaContenedor.classList.add('col-md-4');
+
+            const recetaCard = document.createElement('DIV');
+            recetaCard.classList.add('card', 'mb-4');
+
+            const recetaImagen = document.createElement('IMG');
+            recetaImagen.classList.add('card-img-top');
+            recetaImagen.alt = `Imagen de la receta ${strMeal}`;
+            recetaImagen.src = strMealThumb;
+
+            const recetaCardBody = document.createElement('DIV');
+            recetaCardBody.classList.add('card-body');
+
+            const recetaHeading = document.createElement('H3');
+            recetaHeading.classList.add('card-title', 'mb-3');
+            recetaHeading.textContent = strMeal;
+
+            const recetaButton = document.createElement('BUTTON');
+            recetaButton.classList.add('btn', 'btn-danger', 'w-100');
+            recetaButton.textContent = 'Ver Receta';
+
+
+            // Inyectar en el código HTML
+            recetaCardBody.appendChild(recetaHeading);
+            recetaCardBody.appendChild(recetaButton);
+            
+            recetaCard.appendChild(recetaImagen);
+            recetaCard.appendChild(recetaCardBody);
+
+            recetaContenedor.appendChild(recetaCard);
+
+            resultado.appendChild(recetaContenedor);
+        })
+
+
     }
 }
 
