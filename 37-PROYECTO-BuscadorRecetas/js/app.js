b77a4410cc96e726ea3421cc16363390e9d54400
jhons1206 <webdeveloper84@gmail.com>
2023-02-05 00:25:11 -0500

    Sección 37: PROYECTO: App de Recetas con Fetch API y LocalStorage - Cap. 264

--- 37-PROYECTO-BuscadorRecetas/js/app.js
+++ 37-PROYECTO-BuscadorRecetas/js/app.js
@@ -1,30 +1,29 @@
 function iniciarApp() {
+    const selectCategorias = document.querySelector("#categorias");
+    selectCategorias.addEventListener("change", seleccionarCategoria);
 
-    const selectCategorias = document.querySelector('#categorias');
-    selectCategorias.addEventListener('change', seleccionarCategoria);
-
-    const resultado = document.querySelector('#resultado');
+    const resultado = document.querySelector("#resultado");
+    const modal = new bootstrap.Modal("#modal", {});
 
     obtenerCategorias();
 
     function obtenerCategorias() {
-        const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+        const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
         fetch(url)
-            .then( respuesta => respuesta.json())
-            .then( resultado => mostrarCategorias(resultado.categories))
+            .then((respuesta) => respuesta.json())
+            .then((resultado) => mostrarCategorias(resultado.categories));
     }
 
-    function mostrarCategorias( categorias = []) {
-        categorias.forEach( categoria => {
-            const option = document.createElement('OPTION');
-            // console.log(option);
+    function mostrarCategorias(categorias = []) {
+        categorias.forEach((categoria) => {
+            const option = document.createElement("OPTION");
 
             const { strCategory } = categoria;
 
             option.value = strCategory;
             option.textContent = strCategory;
             selectCategorias.appendChild(option);
-        })
+        });
     }
 
     function seleccionarCategoria(e) {
@@ -32,54 +31,113 @@ function iniciarApp() {
         const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
 
         fetch(url)
-            .then( respuesta => respuesta.json())
-            .then( resultado => mostrarRecetas(resultado.meals))
+            .then((respuesta) => respuesta.json())
+            .then((resultado) => mostrarRecetas(resultado.meals));
     }
 
     function mostrarRecetas(recetas = []) {
+        // Limpiar búsqueda
+        limpiarHTML(resultado);
+
+        const heading = document.createElement("H2");
+        heading.classList.add("text-center", "text-black", "my-5");
+        heading.textContent = recetas.length ? "Resultados" : "No hay resultados";
+        resultado.appendChild(heading);
 
         // Iterar en los resultados
-        recetas.forEach( receta => {
-            // console.log(receta);
+        recetas.forEach((receta) => {
             const { idMeal, strMeal, strMealThumb } = receta;
 
-            const recetaContenedor = document.createElement('DIV');
-            recetaContenedor.classList.add('col-md-4');
+            const recetaContenedor = document.createElement("DIV");
+            recetaContenedor.classList.add("col-md-4");
 
-            const recetaCard = document.createElement('DIV');
-            recetaCard.classList.add('card', 'mb-4');
+            const recetaCard = document.createElement("DIV");
+            recetaCard.classList.add("card", "mb-4");
 
-            const recetaImagen = document.createElement('IMG');
-            recetaImagen.classList.add('card-img-top');
+            const recetaImagen = document.createElement("IMG");
+            recetaImagen.classList.add("card-img-top");
             recetaImagen.alt = `Imagen de la receta ${strMeal}`;
             recetaImagen.src = strMealThumb;
 
-            const recetaCardBody = document.createElement('DIV');
-            recetaCardBody.classList.add('card-body');
+            const recetaCardBody = document.createElement("DIV");
+            recetaCardBody.classList.add("card-body");
 
-            const recetaHeading = document.createElement('H3');
-            recetaHeading.classList.add('card-title', 'mb-3');
+            const recetaHeading = document.createElement("H3");
+            recetaHeading.classList.add("card-title", "mb-3");
             recetaHeading.textContent = strMeal;
 
-            const recetaButton = document.createElement('BUTTON');
-            recetaButton.classList.add('btn', 'btn-danger', 'w-100');
-            recetaButton.textContent = 'Ver Receta';
-
+            const recetaButton = document.createElement("BUTTON");
+            recetaButton.classList.add("btn", "btn-danger", "w-100");
+            recetaButton.textContent = "Ver Receta";
+            // recetaButton.dataset.bsTarget = "#modal";
+            // recetaButton.dataset.bsToggle = "modal";
+            recetaButton.onclick = function () {
+                seleccionarReceta(idMeal);
+            };
 
             // Inyectar en el código HTML
             recetaCardBody.appendChild(recetaHeading);
             recetaCardBody.appendChild(recetaButton);
-            
+
             recetaCard.appendChild(recetaImagen);
             recetaCard.appendChild(recetaCardBody);
 
             recetaContenedor.appendChild(recetaCard);
 
             resultado.appendChild(recetaContenedor);
-        })
+        });
+    }
 
+    function seleccionarReceta(id) {
+        const url = `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
+        fetch(url)
+            .then((respuesta) => respuesta.json())
+            .then((resultado) => mostrarRecetaModal(resultado.meals[0]));
+    }
 
+    function mostrarRecetaModal(receta) {
+        const { idMeal, strInstructions, strMeal, strMealThumb } = receta;
+
+        // Agregar contenido al modal
+        const modalTitle = document.querySelector(".modal .modal-title");
+        const modalBody = document.querySelector(".modal .modal-body");
+
+        modalTitle.textContent = strMeal;
+        modalBody.innerHTML = `
+            <img class="img-fluid" src="${strMealThumb}" alt="receta ${strMeal}" />
+            <h3 class="my-3">Instrucciones</h3>
+            <p>${strInstructions}</p>
+            <h3 class="my-3">Ingredientes y Cantidades</h3>
+        `;
+
+        // Mostrar cantidades e ingredientes
+        const listGroup = document.createElement("UL");
+        listGroup.classList.add("list-group");
+
+        for (let i = 1; i <= 20; i++) {
+            if (receta[`strIngredient${i}`]) {
+                const ingrediente = receta[`strIngredient${i}`];
+                const cantidad = receta[`strMeasure${i}`];
+
+                const ingredienteLi = document.createElement("LI");
+                ingredienteLi.classList.add("list-group-item");
+                ingredienteLi.textContent = `${ingrediente} - ${cantidad}`;
+
+                listGroup.appendChild(ingredienteLi);
+            }
+        }
+
+        modalBody.appendChild(listGroup);
+
+        // Muestra el modal
+        modal.show();
+    }
+
+    function limpiarHTML(selector) {
+        while (selector.firstChild) {
+            selector.removeChild(selector.firstChild);
+        }
     }
 }
 
-document.addEventListener('DOMContentLoaded', iniciarApp);
+document.addEventListener("DOMContentLoaded", iniciarApp);
