b08f43bd4dd8214bb3cd33e2b2e75084952fdee6
jhons1206 <webdeveloper84@gmail.com>
2023-02-12 08:34:00 -0500

    Sección 40: PROYECTO: REST API de Restaurant con Consumos y Propinas - Cap. 296

--- 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -6,6 +6,12 @@ let cliente = {
     pedido: [],
 };
 
+const categorias = {
+    1: 'Comida',
+    2: 'Bebidas',
+    3: 'Postres',
+};
+
 const btnGuardarCliente = document.querySelector('#guardar-cliente');
 btnGuardarCliente.addEventListener('click', guardarCliente);
 
@@ -30,7 +36,77 @@ function guardarCliente() {
                 alerta.remove();
             }, 3000);
         }
-    } else {
-        console.log('Todos los campos están llenos');
+
+        return;
     }
+
+    cliente = { ...cliente, mesa, hora };
+
+    // console.log(cliente)
+
+    // Ocultar Modal
+    const modalFormulario = document.querySelector('#formulario');
+    const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+    modalBootstrap.hide();
+
+    // Mostrar las secciones
+    mostrarSecciones();
+
+    // Obtener platillos de la API de JSON Server
+    obtenerPlatillos();
+}
+
+function mostrarSecciones() {
+    const seccionesOcultas = document.querySelectorAll('.d-none');
+    seccionesOcultas.forEach((seccion) => seccion.classList.remove('d-none'));
+}
+
+function obtenerPlatillos() {
+    const url = 'http://localhost:4000/platillos';
+
+    fetch(url)
+        .then((respuesta) => respuesta.json())
+        .then((resultado) => mostrarPlatillos(resultado))
+        .catch((error) => console.log(error));
+}
+
+function mostrarPlatillos(platillos) {
+    const contenido = document.querySelector('#platillos .contenido');
+
+    platillos.forEach((platillo) => {
+        const { id, nombre, precio, categoria } = platillo;
+
+        const row = document.createElement('DIV');
+        row.classList.add('row', 'py-3', 'border-top');
+
+        const nombrePlatillo = document.createElement('DIV');
+        nombrePlatillo.classList.add('col-md-4');
+        nombrePlatillo.textContent = nombre;
+
+        const precioPlatillo = document.createElement('DIV');
+        precioPlatillo.classList.add('col-md-3', 'fw-bold');
+        precioPlatillo.textContent = `$ ${precio}`;
+
+        const categoriaPlatillo = document.createElement('DIV');
+        categoriaPlatillo.classList.add('col-md-3');
+        categoriaPlatillo.textContent = categorias[categoria];
+
+        const inputCantidad = document.createElement('INPUT');
+        inputCantidad.type = 'number';
+        inputCantidad.min = 0;
+        inputCantidad.id = `producto-${id}`;
+        inputCantidad.classList.add('form-control');
+        inputCantidad.value = 0;
+
+        const agregar = document.createElement('DIV');
+        agregar.classList.add('col-md-2');
+        agregar.appendChild(inputCantidad);
+
+        row.appendChild(nombrePlatillo);
+        row.appendChild(precioPlatillo);
+        row.appendChild(categoriaPlatillo);
+        row.appendChild(agregar);
+
+        contenido.appendChild(row);
+    });
 }
