761e5501a8dab04a47b6284988d83c9b0905648a
jhons1206 <webdeveloper84@gmail.com>
2023-02-13 08:40:42 -0500

    Sección 40: PROYECTO: REST API de Restaurant con Consumos y Propinas - Cap. 300

--- 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -139,8 +139,9 @@ function agregarPlatillo(producto) {
             cliente.pedido = [...pedido, producto];
         }
     } else {
-        console.log('No es mayor a 0');
+        // Eliminar elementos cuando la cantidad es 0
+        const resultado = pedido.filter((articulo) => articulo.id !== producto.id);
+        cliente.pedido = [...resultado];
     }
 
-    console.log(cliente.pedido);
 }
