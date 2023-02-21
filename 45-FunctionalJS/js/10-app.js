d6b05dcafe6fe62a66cf86c333b5e03e74ded258
Jhon Sandoval <webdeveloper84@gmail.com>
2023-02-21 11:59:02 -0500

    Sección 45: Functional JavaScript - COMPLETADO

--- 45-FunctionalJS/js/10-app.js
+++ 45-FunctionalJS/js/10-app.js
@@ -13,6 +13,24 @@ const guardarEmail = (info) => ({
     },
 });
 
+const obtenerEmail = (info) => ({
+    mostrarEmail() {
+        console.log(`Email: ${info.email}`);
+    },
+});
+
+const obtenerEmpresa = info => ({
+    mostrarEmpresa() {
+        console.log(`Empresa: ${info.empresa}`);
+    }
+})
+
+const obtenerPuesto = info => ({
+    mostrarPuesto() {
+        console.log(`Puesto: ${info.puesto}`);
+    }
+})
+
 function Cliente(nombre, email, empresa) {
     let info = {
         nombre,
@@ -20,7 +38,13 @@ function Cliente(nombre, email, empresa) {
         empresa,
     };
 
-    return Object.assign(info, obtenerNombre(info));
+    return Object.assign(
+        info, 
+        obtenerNombre(info),
+        guardarEmail(info),
+        obtenerEmail(info),
+        obtenerEmpresa(info)
+    );
 }
 
 function Empleado(nombre, email, puesto) {
@@ -30,11 +54,28 @@ function Empleado(nombre, email, puesto) {
         puesto,
     };
 
-    return Object.assign(info, obtenerNombre(info));
+    return Object.assign(
+        info, 
+        obtenerNombre(info),
+        guardarEmail(info),
+        obtenerEmail(info),
+        obtenerPuesto(info)
+    );
 }
 
 const cliente = Cliente('Jhon', 'jhon@gmail.com', 'Maditek Soft');
+console.log('Datos Cliente:')
+console.log('==============')
 cliente.mostrarNombre();
+cliente.agregarEmail('cliente@cliente.com');
+cliente.mostrarEmail();
+cliente.mostrarEmpresa();
+console.log('\n');
 
+console.log('Datos Empleado:')
+console.log('==============')
 const empleado = Empleado('Pedro', 'empleado@correo.com', 'Programador');
 empleado.mostrarNombre();
+empleado.agregarEmail('empleado@empleado.com');
+empleado.mostrarEmail();
+empleado.mostrarPuesto();
